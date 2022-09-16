13eb4557c628a01e2ba7b5e55011a944ff8c4f48
ebdiego <112005554+ebdiego@users.noreply.github.com>
2022-09-15 22:28:24 -0400

    Add files via upload

---
+++ fe1.js
@@ -0,0 +1,45 @@
+//Eberto Diego
+// N220
+// 09/15/2022
+
+let car = {
+x: 200, y: 50
+}
+
+//Object created
+
+function setup() {
+    createCanvas(720, 480);
+
+}
+
+function drawCar() {
+    circle(car.x, car.y, mouseX, mouseY);
+}
+
+
+let randColor = {
+    color (170, 200, 150);
+}
+
+// Color of choice
+
+function removeRed() {
+     randColor.setRed(0); 
+
+    return noRed;
+}
+
+// remove red from color
+
+function polarPoint() {
+
+    var x =  r * Math.sin(mouseX);
+    var y = r * Math.cos(mouseX);
+
+// draw coordinates
+
+    return createVector(x,y); 
+
+
+}
