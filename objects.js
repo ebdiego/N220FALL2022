3cb733714064e12ea27dd8ecc1c4ab8930e31726
ebdiego <112005554+ebdiego@users.noreply.github.com>
2022-09-22 23:47:58 -0400

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,68 @@
+let bouncyBall = {
+color: "#FF0000",
+width: 20,
+height: 10,
+x: 200,
+y: 500
+}
+
+
+function drawBall() {
+
+
+
+    background(220);
+
+
+    circle(bouncyBall.x, bouncyBall.y, bouncyBall.width, bouncyBall.height);
+
+
+
+}
+
+
+let rectangle = {
+
+width: 50,
+height: 100,
+x: 200,
+y:100,
+
+
+}
+
+let rectangle2 = {
+
+
+    width: 50,
+    height: 100,
+    x:600,
+    y:100,
+
+}
+
+
+function setup () {
+
+createCanvas(800, 500);
+
+
+}
+
+
+
+function drawRect() {
+
+    rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    rect(rectangle2.x, rectangle2.y, rectangle2.width, rectangle2.height);
+
+
+
+    if (keyIsDown(UP_ARROW)) {
+        //change properties
+      }
+
+      if (keyIsUp(DOWN_ARROW)) {
+        //change properties
+      }
+}
